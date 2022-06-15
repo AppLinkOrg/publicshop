4e1688e17e4fec7b7e5e946582c6f5b81da7fcae
蔡小司 <alucard263096@126.com>
2022-06-15 16:53:37 +0800

    commit

--- minibase/source/apis/apiutil.js
+++ minibase/source/apis/apiutil.js
@@ -15,7 +15,7 @@ export class ApiUtil {
 
 
     s = s.replace(new RegExp("</p>", "gm"), "</p><br />");
-    s = s.replace(new RegExp("\"/alucard263096/publicshop/upload/", "gm"), "\"" + "https://cmsdev.app-link.org/alucard263096/publicshop/upload/");
+    s = s.replace(new RegExp("\"/upload/", "gm"), "\"" + "https://shenlongcms.zxtriple.com/upload/");
 
 
     return s;
